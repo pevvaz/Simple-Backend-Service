5445bdad4edca87180d94371b10114c1bc7fbf2e
pevvaz <pevvaz0@gmail.com>
2026-01-12 15:52:58 -0300

    Backup, before fix the project paths

--- Backend/Servico Backend/Controllers/UsersController.cs
+++ Backend/Servico Backend/Controllers/UsersController.cs
@@ -15,36 +15,75 @@ public class UsersController : ControllerBase
         _usersContext = usersContext;
     }
 
-    [HttpGet(template: "{role?}/list/all")]
-    public async Task<IActionResult> ListUsers([FromRoute] int? id, [FromRoute] string role = "customer")
+    [HttpGet(template: "/list/all")]
+    public async Task<IActionResult> ListAllUsersAction()
     {
-        if (Enum.TryParse(role, true, out UsersModels.Roles parseRole))
+        var list = await _usersContext.Users.AsNoTracking().ToListAsync();
+
+        return Ok();
+    }
+
+    [HttpGet(template: "/list/all/{role?}")]
+    public async Task<IActionResult> ListUsersAction([FromRoute] int? id, [FromRoute] string role = "customer")
+    {
+        if (role == null)
         {
-            var list = await _usersContext.Users.AsNoTracking().Where(u => u.Role == parseRole).ToListAsync();
-            return Ok(list);
+            return RedirectToAction(nameof(ListAllUsersAction));
         }
         else
         {
-            return BadRequest($"The following Role doesn't exist: {role}");
-        }
-    }
-    [HttpGet(template: "{role}/list/{id:int?}")]
-    public async Task<IActionResult> ListUsersId([FromRoute] int id, [FromRoute] string role = "customer")
-    {
-        if (Enum.TryParse(role, true, out UsersModels.Roles parseRole))
-        {
-            var user = await _usersContext.Users.AsNoTracking().FirstAsync(u => u.Role == parseRole && u.Id == id);
-
-            if (user == null)
+            if (Enum.TryParse(role, true, out UsersModels.Roles parseRole))
             {
-                return NotFound($"An User of Id: {id} and Role: {role} doesn't exist.");
-            }
+                var list = await _usersContext.Users.AsNoTracking().Where(u => u.Role == parseRole).ToListAsync();
 
+                return Ok(list);
+            }
+            else
+            {
+                return BadRequest($"The following Role doesn't exist: {role}");
+            }
+        }
+
+    }
+
+    [HttpGet(template: "/list/{id:int}")]
+    public async Task<IActionResult> ListUsersIdAction([FromRoute] int id, [FromRoute] string role = "customer")
+    {
+        var user = await _usersContext.Users.AsNoTracking().FirstAsync(u => u.Id == id);
+
+        if (user != null)
+        {
             return Ok(user);
         }
         else
         {
-            return BadRequest($"The following Role doesn't exist: {role}");
+            return NotFound($"An User of Id: {id} was not found");
         }
+    }
+
+    [HttpPost(template: "/create/")]
+    public async Task<IActionResult> CreateAction([FromBody] UsersModels user)
+    {
+        if (user != null)
+        {
+            await _usersContext.Users.AddAsync(user);
+            await _usersContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(ListUsersIdAction), new { id = user.Id }, user);
+        }
+        else
+        {
+            return BadRequest();
+        }
+    }
+
+    [HttpDelete(template: "/delete/{id:int}")]
+    public async Task<IActionResult> DeleteAction([FromRoute] int id)
+    {
+        var user = await _usersContext.Users.FirstAsync(u => u.Id == id);
+        _usersContext.Users.Remove(user);
+        await _usersContext.SaveChangesAsync();
+
+        return Ok();
     }
 }
